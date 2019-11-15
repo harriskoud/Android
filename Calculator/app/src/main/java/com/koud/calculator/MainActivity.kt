dabe28cf97fc86a241baee22c9874bf6592ab7cd
Charidimos Koudoumas <extern.charidimos.koudoumas@elektrobit.com>
2019-11-15 16:37:40 +0200

    calculator app

---
+++ Calculator/app/src/main/java/com/koud/calculator/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.koud.calculator
+
+import android.os.Bundle
+import android.view.View.OnClickListener
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+private const val STATE_PENDING_OPERATION = "PendingOperation"
+private const val STATE_OPERAND1 = "Operand1"
+private const val STATE_OPERAND1_STORED = "Operand1_Stored"
+
+class MainActivity : AppCompatActivity() {
+
+    private var operand1: Double? = null
+    private var pendingOperation = "="
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val numberListener = OnClickListener()
+        {
+            val button = (it as Button)
+            newNumber.append(button.text)
+        }
+
+        button0.setOnClickListener(numberListener)
+        button1.setOnClickListener(numberListener)
+        button2.setOnClickListener(numberListener)
+        button3.setOnClickListener(numberListener)
+        button4.setOnClickListener(numberListener)
+        button5.setOnClickListener(numberListener)
+        button6.setOnClickListener(numberListener)
+        button7.setOnClickListener(numberListener)
+        button8.setOnClickListener(numberListener)
+        button9.setOnClickListener(numberListener)
+
+        val opListener = OnClickListener { v ->
+            val op = (v as Button).text.toString()
+            try {
+                val value = newNumber.text.toString().toDouble()
+                performOperation(value, op)
+            } catch (e: NumberFormatException) {
+                newNumber.setText("")
+            }
+            pendingOperation = op
+            operation.text = pendingOperation
+        }
+
+        buttonPlus.setOnClickListener(opListener)
+        buttonMinus.setOnClickListener(opListener)
+        buttonDivide.setOnClickListener(opListener)
+        buttonMul.setOnClickListener(opListener)
+        buttonEquals.setOnClickListener(opListener)
+
+
+    }
+
+    private fun performOperation(value: Double, operation: String) {
+        if (operand1 == null) {
+            operand1 = value
+        } else {
+            if (pendingOperation == "=") {
+                pendingOperation = operation
+            }
+
+            when (pendingOperation) {
+                "=" -> operand1 = value
+                "/" -> operand1 = if (value == 0.0) {
+                    Double.NaN   // handle attempt to divide by zero
+                } else {
+                    operand1!! / value
+                }
+                "*" -> operand1 = operand1!! * value
+                "-" -> operand1 = operand1!! - value
+                "+" -> operand1 = operand1!! + value
+            }
+        }
+        result.setText(operand1.toString())
+        newNumber.setText("")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (operand1 != null) {
+            outState.putDouble(STATE_OPERAND1, operand1!!)
+            outState.putBoolean(STATE_OPERAND1_STORED, true)
+        }
+        outState.putString(STATE_PENDING_OPERATION, pendingOperation)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        operand1 = if (savedInstanceState.getBoolean(STATE_OPERAND1_STORED, false)) {
+            savedInstanceState.getDouble(STATE_OPERAND1)
+        } else {
+            null
+        }
+
+        pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION)
+        operation.text = pendingOperation
+    }
+}
